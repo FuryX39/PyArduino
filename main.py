07f2a750b1fd4625b2e131610035a2f811354873
FuryX39 <58467715+FuryX39@users.noreply.github.com>
2021-04-06 14:51:22 +0300

    'Initial'

---
+++ main.py
@@ -0,0 +1,61 @@
+from random import randint
+
+import serial
+import time
+
+leftHand = serial.Serial('com1', 9600)
+rightHand = serial.Serial('com2', 9600)
+time.sleep(2)
+print(leftHand.readline())
+print(rightHand.readline())
+
+
+def Action(impAction: str):
+    """Передается действие в виде строки
+    При передаче действия Hello рандомно определяется вид приветствия - рукопожатие или брофист
+    При передаче неизвестного действия выполняется Unknown action"""
+
+    if impAction == 'Hello':
+
+        helloType = str(randint(0, 1))
+
+        if helloType == '0':
+            print('Python response: Brofist action')
+        else:
+            print('Python response: Handshake action')
+
+        rightHand.write(helloType.encode())
+        print(rightHand.readline())
+
+        leftHand.write(helloType.encode())
+        print(leftHand.readline())
+
+    elif impAction == 'Pathfinder':
+
+        print('Python response: Pathfinder action')
+
+        rightHand.write('2'.encode())
+        print(rightHand.readline())
+
+        leftHand.write('2'.encode())
+        print(leftHand.readline())
+
+    elif impAction == 'Bye':
+
+        print('Python response: Bye action')
+
+        rightHand.write('3'.encode())
+        print(rightHand.readline())
+
+        leftHand.write('3'.encode())
+        print(leftHand.readline())
+
+    else:
+
+        print('Python response: Unknown action')
+
+        rightHand.write('4'.encode())
+        print(rightHand.readline())
+
+        leftHand.write('4'.encode())
+        print(leftHand.readline())
